e6bdf12385957a0af0907b61d70abcf3f2980b30
BigBadRussian <skripkoae@gmail.com>
2024-04-22 16:27:37 +0400

    move os.environ to main func build_table.py

--- Build_table.py
+++ Build_table.py
@@ -1,4 +1,7 @@
 from __future__ import print_function
+
+import os
+
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
 from Programmers_Salaries_stats_HH import get_vacancies_statistics_hh
@@ -21,9 +24,11 @@ def build_table(hr_service_statistics: dict, title: str):
 
 def main():
     load_dotenv()
-    print(build_table(hr_service_statistics=get_vacancies_statistics_hh(),
+    user_agent_hh = os.environ['USER_AGENT_HH']
+    app_secret_key_sj = os.environ['APP_SECRET_KEY_SJ']
+    print(build_table(hr_service_statistics=get_vacancies_statistics_hh(user_agent_hh),
                       title='HeadHunter Moscow'),
-          build_table(hr_service_statistics=get_vacancies_statistics_sj(),
+          build_table(hr_service_statistics=get_vacancies_statistics_sj(app_secret_key_sj),
                       title='SuperJob Moscow'))
 
 
