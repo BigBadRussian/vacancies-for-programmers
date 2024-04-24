e8545631ed0bd0df8dd4df6735ea97b94be9aeb7
BigBadRussian <skripkoae@gmail.com>
2024-04-24 17:16:51 +0400

    move all salary operations into calculate_average_salary function remove imports from hh script

--- Programmers_Salaries_stats_SJ.py
+++ Programmers_Salaries_stats_SJ.py
@@ -1,12 +1,9 @@
 import math
 import requests
-from itertools import chain
-from Programmers_Salaries_stats_HH import calculate_average_salaries
-from Programmers_Salaries_stats_HH import calculate_expected_rub_salaries
 import logging
 
 
-def request_vacancies(app_secret_key_sj: str, search_word: str):
+def request_vacancies_sj(app_secret_key_sj: str, language: str):
     url = '	https://api.superjob.ru/2.0/vacancies/'
     headers = {'X-Api-App-Id': f'{app_secret_key_sj}'}
     page = 0
@@ -14,50 +11,52 @@ def request_vacancies(app_secret_key_sj: str, search_word: str):
     vacancies_per_page = 100
     profession_number_sj = 48
     vacancy_lifetime_sj = 0
-    response = []
+    vacancies = []
     while page < pages_number:
-        params = {'catalogues': profession_number_sj, 'keyword': search_word, 'count': f'{vacancies_per_page}',
+        params = {'catalogues': profession_number_sj, 'keyword': language, 'count': f'{vacancies_per_page}',
                   'period': vacancy_lifetime_sj, 'page': page}
         page_response = requests.get(url=url, headers=headers, params=params)
         page_response.raise_for_status()
         page_payload = page_response.json()
         pages_number = math.ceil(page_payload['total'] / vacancies_per_page)
         page += 1
-        response.append(page_payload['objects'])
+        vacancies.extend(page_payload['objects'])
         logging.basicConfig(level=logging.INFO)
-        logging.info('Request SJ')
-    return response
+        logging.info('Request SJ API')
+    return vacancies
 
 
-def get_vacancies_amount_and_payments(sj_response_vacancies: dict, search_words: list):
-    payments = {}
-    vacancies_amount = {}
-    for search_word in search_words:
-        payments[search_word] = [{'payment_from': item['payment_from'],
-                                  'payment_to': item['payment_to'],
-                                  'currency': item['currency']}
-                                 for item in sj_response_vacancies[search_word]]
-        vacancies_amount[search_word] = len(payments[search_word])
-    return payments, vacancies_amount
-
-
-def get_vacancies_statistics_sj(app_secret_key_sj):
-    search_words = ['rust', 'ruby', 'python', 'java_script', 'java', 'c', 'c++', 'c#', 'go']
+def calculate_average_salaries_sj(vacancies: list):
     currency = 'rub'
-    vacancies_statistics = {}
-    sj_response_vacancies = {}
-    try:
-        for search_word in search_words:
-            vacancies = list(chain(*request_vacancies(app_secret_key_sj, search_word)))
-            sj_response_vacancies[search_word] = vacancies
+    expected_salaries = []
+    for vacancy in vacancies:
+        payment_from = vacancy['payment_from']
+        payment_to = vacancy['payment_to']
+        payment_currency = vacancy['currency']
+        if payment_currency != currency:
+            continue
+        if payment_from and payment_to:
+            expected_salaries.append(0.5 * (payment_from + payment_to))
+            continue
+        if payment_from:
+            expected_salaries.append(1.2 * payment_from)
+            continue
+        if payment_to:
+            expected_salaries.append(0.8 * payment_to)
+    vacancies_processed = len(expected_salaries)
+    if vacancies_processed:
+        average_salary = round(sum(expected_salaries) / vacancies_processed, 0)
+    else:
+        average_salary = 'no vacancies found'
+    return average_salary, vacancies_processed
 
-        payments, vacancies_amount = get_vacancies_amount_and_payments(sj_response_vacancies, search_words)
-        expected_salaries = calculate_expected_rub_salaries(payments, search_words, currency)
-        average_salaries = calculate_average_salaries(search_words, expected_salaries)
-        vacancies_statistics = {search_word: {'average_salary': average_salary,
-                                              'vacancies_processed': len(expected_salaries[search_word]),
-                                              'vacancies_amount': vacancies_amount[search_word]}
-                                for search_word, average_salary in zip(search_words, average_salaries)}
-    except requests.ConnectionError:
-        print('Проверьте ваше подключение к сети Интернет')
-    return vacancies_statistics
+
+def get_vacancies_statistics_sj(app_secret_key_sj, languages):
+    hr_statistics_sj = {}
+    for language in languages:
+        vacancies = request_vacancies_sj(app_secret_key_sj, language)
+        average_salary, vacancies_processed = calculate_average_salaries_sj(vacancies)
+        hr_statistics_sj[language] = {'vacancies_amount': len(vacancies),
+                                      'vacancies_processed': vacancies_processed,
+                                      'average_salary': average_salary}
+    return hr_statistics_sj
