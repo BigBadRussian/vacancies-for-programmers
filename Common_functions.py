251b10bc27aa18003748d1432aa6e3b5228932fb
BigBadRussian <skripkoae@gmail.com>
2024-04-28 17:22:41 +0400

    calc_expected_payment return expected_payment without loop

--- Common_functions.py
+++ Common_functions.py
@@ -1,13 +1,10 @@
-def calc_expected_payment(currency, payment_currency, payment_from, payment_to, expected_salaries):
-    while True:
-        if payment_currency != currency:
-            break
-        if not payment_to:
-            expected_salaries.append(1.2 * payment_from)
-            break
-        if not payment_from:
-            expected_salaries.append(0.8 * payment_to)
-            break
-        expected_salaries.append(0.5 * (payment_from + payment_to))
-        break
-    return expected_salaries
+def calc_expected_payment(currency, payment_currency, payment_from, payment_to):
+    if payment_currency != currency:
+        expected_payment = None
+    elif not payment_to:
+        expected_payment = 1.2 * payment_from
+    elif not payment_from:
+        expected_payment = 0.8 * payment_to
+    else:
+        expected_payment = 0.5 * (payment_from + payment_to)
+    return expected_payment
